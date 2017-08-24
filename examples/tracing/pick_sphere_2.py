3112f9dfa7037dbc5b00ad470fb7cf013a4b8409
Eelco Hoogendoorn <hoogendoorn.eelco@gmail.com>
2017-08-24 08:58:12 +0200

    expanded sphere picking example

--- examples/tracing/pick_sphere_2.py
+++ examples/tracing/pick_sphere_2.py
@@ -1,6 +1,4 @@
-"""Visualize harmonics on an irregular simpicial grid
-
-This demonstrates the simplex picking functionality
+"""This demonstrates the spherical simplex picking functionality
 """
 
 
@@ -30,10 +28,7 @@ p0 = perlin_noise(
     ]
 )
 
-# # generate some interesting pattern
-# v = get_harmonics_0(quad)
-# f0 = v[:, 8]
-# vmin, vmax = f0.min(), f0.max()
+vmin, vmax = p0.min(), p0.max()
 
 # pick a rectangular grid of values
 N = 1024
@@ -42,36 +37,33 @@ z = np.sqrt(np.clip(1 - linalg.dot(points, points), 0, 1))
 points = np.concatenate([points, z[..., None]], axis=-1)
 
 
-# if False:
-#     # primal pick compared to primal plot
-#     quad.plot_primal_0_form(f0, plot_contour=False, shading='gouraud', vmin=vmin, vmax=vmax)
-#     quad.plot(ax=plt.gca())
-#     # plt.show()
-#
-#
-#     tri_idx, bary = quad.pick_primal(points.reshape(-1, 2))
-#     plt.figure()
-#     plt.imshow(np.flip(np.moveaxis(bary.reshape(N, N, 3), 0, 1), axis=0))
-#     plt.figure()
-#     plt.imshow(tri_idx.reshape(N, N))
-#
-#     plt.figure()
-#     img = quad.sample_primal_0(f0, points.reshape(-1, 2)).reshape(N, N)
-#     plt.imshow(img.T[::-1])
-#
-#
-#     plt.show()
+if True:
+    # primal pick compared to primal plot
+    sphere.as_euclidian().plot_primal_0_form(p0, plot_contour=False, shading='gouraud', vmin=vmin, vmax=vmax)
+    sphere.plot(ax=plt.gca())
+    # plt.show()
+
+    tri_idx, bary = sphere.pick_primal(points.reshape(-1, 3))
+    plt.figure()
+    plt.imshow(np.flip(np.moveaxis(bary.reshape(N, N, 3), 0, 1), axis=0))
+    plt.figure()
+    plt.imshow(tri_idx.reshape(N, N))
+
+    plt.figure()
+    img = sphere.sample_primal_0(p0, points.reshape(-1, 3)).reshape(N, N)
+    plt.imshow(img.T[::-1])
+
+    plt.show()
 
 
 if True:
     # # interpolate primal 0 form to test dual 0-form plotting; easier than implementing harmonics with BCs's
-    d0 = sphere.topology.averaging_operators_0[2] * p0
+    d0 = sphere.averaging_operators_0[2] * p0
     # sphere.as_euclidian().plot_primal_2_form(d0)
     # plt.show()
 
 
     sphere.as_euclidian().plot_dual_0_form_interpolated(d0, weighted=True, plot_contour=False, shading='gouraud')#, vmin=vmin, vmax=vmax)
-    # quad.plot_dual_0_form_interpolated(d0, weighted=False, plot_contour=False, shading='gouraud', vmin=vmin, vmax=vmax)
     # quad.plot()#ax=plt.gca())
 
     domain_idx, bary, domain = sphere.pick_fundamental(points.reshape(-1, 3))
