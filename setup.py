43211e23c91885686edb48249462e1f7a79bc0d1
Eelco Hoogendoorn <hoogendoorn.eelco@gmail.com>
2017-09-03 09:45:19 +0200

    fixed building of package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='pycomplex',
+    version='0.0.1',
+    packages=find_packages(),
+    license='LGPL',
+    # long_description=open('README.md').read(),
+    platforms='any',
+
+)
