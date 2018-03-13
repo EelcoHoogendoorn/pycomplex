fixed gradient computation

examples/elasticity/seismic.py
@@ -140,11 +140,11 @@ if __name__ == '__main__':
         def step(p, sigma, pos=[0, 0], dir=[-1, 0]):
             return scipy.special.erfc(np.dot(p - pos, dir) / sigma) / 2
 
-        d = step(pp, sigma=complex.metric[1][1].mean() / 2) + 0.01
+        d = circle(pp, sigma=complex.metric[1][1].mean() / 2) + 0.01
         # d = np.ones_like(d)
         m, r, l = [(o * d) for o in complex.topology.averaging_operators_0[-3:]]
         # r = np.ones_like(r)
-        m *= .4
+        m *= .4     # mu is shear stiffness
         if False:
             tris = complex.subdivide_simplicial()
             field = tris.topology.transfer_operators[0] * d
@@ -163,7 +163,7 @@ if __name__ == '__main__':
         p = complex.topology.chain(1, dtype=np.float)
         v = complex.topology.chain(1, dtype=np.float)
         idx = 0
-        idx = np.argmin(np.linalg.norm(complex.dual_position[1] - [0.1, 0], axis=1))
+        idx = np.argmin(np.linalg.norm(complex.dual_position[1] - [0.35, 0], axis=1))
         p[idx] = .01
         # smooth impulse a little since the high frequency components are visually distracting
         for i in range(10):
@@ -186,6 +186,11 @@ if __name__ == '__main__':
             complex.copy(vertices=complex.primal_position[0] + dp * d[:, None]).plot(plot_dual=False)
 
         ax = plt.gca()
+
+        a = np.linspace(0, np.pi*2, endpoint=True)
+        c = np.array([np.cos(a), np.sin(a)]) * 0.4
+        plt.plot(*c)
+
         ax.set_xlim(*complex.box[:, 0])
         ax.set_ylim(*complex.box[:, 1])
         plt.axis('off')

pycomplex/complex/base.py
@@ -344,19 +344,28 @@ class BaseComplex(object):
         This is the docstring of the wrapped inner function
         """
         assert self.is_pairwise_delaunay    # required since we are dividing by dual edge lengths; does not work for cubes yet
+        B = self.topology._boundary[-1]
+        O = self.topology._orientation[-1]
+        B = B.reshape(len(B), -1)
+        O = O.reshape(len(O), -1)
 
         from pycomplex.complex.simplicial.euclidian import ComplexSimplicialEuclidian
+        from pycomplex.complex.regular import ComplexRegularMixin
         if isinstance(self, ComplexSimplicialEuclidian):
             from pycomplex.geometry import euclidian
             # FIXME: this only works for simplices; or can it be generalized to cubes too? gradients are per vertex-opposing-face pair;
             # FIXME can think of it as volume gradient of moving vertex, or gradient of moving opposing face
             # FIXME: analogous computation would be normal of cube faces multiplied with their area
             gradients = euclidian.simplex_gradients(self.vertices[self.topology.elements[-1]])
-        else:
-            q = self.vertices[self.topology.corners[-1]]
-            q -= q.mean(axis=1, keepdims=True)
-            # FIXME: this only is true for square cubes
-            gradients = q
+
+        elif isinstance(self, ComplexRegularMixin):
+            pp = self.primal_position
+            gradients = pp[-2][B] - pp[-1][:, None, :]
+            # gradients -= gradients.mean(axis=1, keepdims=True)
+
+            gradients = linalg.normalized(gradients)
+            gradients *= self.primal_metric[-2][B][..., None]
+
 
         u, s, v = np.linalg.svd(gradients)
         # only retain components in the plane of the element
@@ -369,10 +378,6 @@ class BaseComplex(object):
         # # # which projected on the dual edges forms the dual fluxes. on a sphere the third component is not determined
         # # # approximate inverse would still make sense in cubical topology however
         # # # tangent_directions.dot(velocity) = tangent_velocity_component
-        B = self.topology._boundary[-1]
-        O = self.topology._orientation[-1]
-        B = B.reshape(len(B), -1)
-        O = O.reshape(len(O), -1)
 
         def block_diag(blocks):
             b, r, c = np.indices(blocks.shape)
@@ -387,7 +392,7 @@ class BaseComplex(object):
             return scipy.sparse.coo_matrix((O.flatten(), (r.flatten(), c.flatten())))
 
         bpinv = block_diag(pinv)
-        s = signed_selector(B.flatten(), O.flatten())
+        s = signed_selector(B.flatten(), O.flatten())       # this assembles all fluxes of each n-cube in 2*n rows per n_cube
         pd1 = scipy.sparse.diags(self.hodge_PD[1])
         core = (bpinv * s * pd1).tocsr()    # perhaps drop near-zero terms?
         # NOTE: dual selectors are indexed by primal element order!

pycomplex/geometry/regular.py
@@ -8,9 +8,50 @@ def edge_length(s, e):
 
 
 def hypervolume(a, b, n=None):
-    """Compute unsigned hypervolume"""
+    """Compute unsigned hypervolume
+
+    Parameters
+    ----------
+    a : ndarray, [n_cubes, n_dim], float
+        one corner of the cubes
+    b : ndarray, [n_cubes, n_dim], float
+        opposing corner of the cubes
+    n : int, optional
+        to compute the restricted hypervolume of dimension n for cubes embedded in higher dimensional spaces
+
+    Returns
+    -------
+    ndarray, [n_cubes], float
+    """
     d = a - b
     if n is not None:
         d = np.sort(np.abs(d), axis=1)
         d = d[:, -n:]
     return np.abs(np.prod(d, axis=-1))
+
+
+# def vertex_gradients(cubes):
+#     """Gradient of hypervolume with respect to displacement along the vertices of each cube
+#
+#     Parameters
+#     ----------
+#     cubes, [n_cubes, (2,)**n_dim, n_dim], float
+#
+#     Returns
+#     -------
+#     gradients, [n_cubes, (2,)**n_dim, n_dim], float
+#     """
+#
+#     # cubes = cubes.reshape(len(cubes), -1)
+#     gradients = cubes - cubes.mean(axis=np.arange(cubes.ndim)[1:-1], keepdims=True)
+#     # FIXME: this only is true for square cubes
+#     return gradients
+#
+#
+# def boundary_gradients(cubes):
+#     """Gradient of hypervolume with respect to displacement along the bounding faces of each cube
+#
+#     Parameters
+#     ----------
+#
+#     """