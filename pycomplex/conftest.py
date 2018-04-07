d73672519d0634851e33a8e2a3189fc8167de67f
EelcoHoogendoorn <hoogendoorn.eelco@gmail.com>
2018-04-07 21:38:47 +0200

    other method of suppressing plots on ci

--- pycomplex/conftest.py
+++ pycomplex/conftest.py
@@ -1,7 +1,6 @@
 
-
-
 import pytest
+
 
 def pytest_addoption(parser):
     parser.addoption("--show_plot", action="store", default="True",
@@ -12,7 +11,11 @@ def pytest_addoption(parser):
 def show_plot(request):
     """This callable fixture allows us to either show plots when running test for visual inspection,
     or close them and at least test the absence of exceptions when running test automated"""
-    flag = request.config.getoption("--show_plot")
+    try:
+        flag = request.config.getoption("--show_plot")
+    except:
+        import os
+        flag = os.environ['SHOW_PLOT']
     import matplotlib.pyplot as plt
     if flag == 'True':
         return plt.show
