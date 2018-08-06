expanded test suite and fixed dual derivative

pycomplex/stencil/complex.py
@@ -14,8 +14,12 @@ class StencilComplex(object):
     The defining distinction of the stencil based approach is that all topology is implicit
     other than its shape it has no associated memory requirements
 
+    TODO
+    ----
     make component-first or component-last configurable?
 
+    emit code instead of making callables?
+    might eliminate quite some overhead and expose parralelism?
     """
 
     def __init__(self, shape: Tuple[int], scale=1, boundary: str = 'periodic'):
@@ -60,6 +64,8 @@ class StencilComplex(object):
             return np.zeros((components, ) + self.shape, dtype=dtype)
         if init == 'empty':
             return np.empty((components, ) + self.shape, dtype=dtype)
+        if init == 'ones':
+            return np.ones((components, ) + self.shape, dtype=dtype)
         raise Exception
 
     @property
@@ -77,7 +83,7 @@ class StencilComplex(object):
         def corr(*args, **kwargs):
             return ndimage.correlate1d(*args, **kwargs, mode='wrap')
 
-        def derivative(n, symbols, terms, axes, parities, func):
+        def primal(n, symbols, terms, axes, parities):
             """Exterior derivative operator"""
             def inner(f):
                 d = self.form(n + 1, init='empty')
@@ -90,17 +96,49 @@ class StencilComplex(object):
                         i = symbols.index(t)
                         weights = [-1, +1] if p else [+1, -1]
                         if initialized[c]:
-                            d[c] += func(f[i], axis=a, weights=weights)
+                            d[c] += conv(f[i], axis=a, weights=weights)
                         else:
-                            func(f[i], axis=a, output=d[c], weights=weights)
+                            conv(f[i], axis=a, output=d[c], weights=weights)
                             initialized[c] = True
+                return d
+            return inner
+        def dual(n, symbols, terms, axes, parities):
+            """Exterior derivative operator"""
+            def inner(f):
+                """
+
+                Parameters
+                ----------
+                f:
+                    n+1-form
+
+                Returns
+                -------
+                n-form
+                """
+                d = self.form(n, init='empty')
+                # write once; update afterwards
+                initialized = [False] * len(d)
+                # loop over all components of the new derived form
+                for c, (T, A, P) in enumerate(zip(terms, axes, parities)):
+                    # loop over all terms that the new symbol is composed of
+                    for t, a, p in zip(T, A, P):
+                        i = symbols.index(t)
+                        weights = [-1, +1] if p else [+1, -1]
+                        if initialized[i]:
+                            d[i] += corr(f[c], axis=a, weights=weights)
+                        else:
+                            corr(f[c], axis=a, output=d[i], weights=weights)
+                            initialized[i] = True
                 return d
             return inner
 
         return [
             StencilOperator(
-                left=derivative(n, symbols, terms, axes, parities, conv),
-                right=derivative(n, symbols, terms, axes, parities, corr),
+                # in boundary-free case, left operator equals dual derivative
+                # not just a conv/corr difference; also need to transpose summing logic!
+                left=dual(n, symbols, terms, axes, parities),
+                right=primal(n, symbols, terms, axes, parities),
                 shape=(self.form(n + 1).shape, self.form(n).shape)
             )
             for n, (symbols, terms, axes, parities)
@@ -136,19 +174,9 @@ class StencilComplex(object):
         -----
         only pure regular grids now; add support for more complex metrics?
         """
-        def h(n):
-            scale = (self.scale ** (self.ndim - n * 2))
-            def inner(x):
-                return x * scale
-            return inner
-        def hi(n):
-            scale = (self.scale ** (self.ndim - n * 2))
-            def inner(x):
-                return x / scale
-            return inner
         return [
             DiagonalOperator(
-                h(n), hi(n),
+                self.scale ** (self.ndim - n * 2),
                 self.form(n).shape,
             )
             for n in range(self.ndim + 1)
@@ -166,9 +194,12 @@ class StencilComplex(object):
             sm = smoother(1)
             if not scale:
                 sm = sm / sm.max()
+            else:
+                sm
 
             def inner(f):
                 """smooth a primal n-form along directions normal to it"""
+                # FIXME: for n-form we need no smoothing and we can exit early without copy
                 smoothed = np.copy(f)
                 # loop over all components of the form
                 for c, s in enumerate(symbols):
@@ -225,7 +256,7 @@ class StencilComplex(object):
             return inner
 
         return [
-            # NOTE: if  scaling were to be absorbed here if would not longer be a true transpose relationship
+            # NOTE: this is not a true transpose relationship when the call to binning is using a mean reduction
             StencilOperator(
                 left=tile(n),
                 right=bin(n),
@@ -236,38 +267,38 @@ class StencilComplex(object):
 
     @property
     def coarsen(self):
-        """
+        """Coarsen operators, mapping self to self.coarse
+
         Returns
         -------
         array_like, [ndim + 1], Operator
+            n-th operator maps primal self.form(n) to primal self.coarse.form(n)
 
         Notes
         -----
-        transpose of refine exacept for scale
+        transpose of refine except for a scale factor
         """
-        T = self.transfers
-        S = self.smoothers(scale=True)
         return [
-            ComposedOperator(T[n], S[n])
-            for n in range(self.ndim + 1)
+            ComposedOperator(T, S)
+            for T, S in zip(self.transfers, self.smoothers(scale=True))
         ]
 
     @property
     def refine(self):
-        """
+        """Refinement operators, mapping self.coarse to self
+
         Returns
         -------
         array_like, [ndim + 1], Operator
+            n-th operator maps primal self.coarse.form(n) to primal self.form(n)
 
         Notes
         -----
-        transpose of coarsen exacept for scale
+        transpose of coarsen except for a scale factor
         """
-        T = self.transfers
-        S = self.smoothers(scale=False)
         return [
-            ComposedOperator(T[n], S[n]).transpose
-            for n in range(self.ndim + 1)
+            ComposedOperator(T, S).transpose
+            for T, S in zip(self.transfers, self.smoothers(scale=False))
         ]
 
 

pycomplex/stencil/operator.py
@@ -1,4 +1,5 @@
 from typing import Tuple
+import numpy as np
 
 
 class StencilOperator(object):
@@ -52,17 +53,18 @@ class SymmetricOperator(object):
 
 
 class DiagonalOperator(SymmetricOperator):
-    def __init__(self, op: callable, inv: callable, shape: Tuple):
-        self.left = op
-        self.right = op
-        self.inv = inv
+    def __init__(self, diagonal: np.ndarray, shape: Tuple):
+        self.diagonal = diagonal
         self.shape = shape, shape
 
     @property
     def inverse(self):
         return DiagonalOperator(
-            self.inv, self.left, self.shape[0]
+            1. / self.diagonal, self.shape[0]
         )
+
+    def __call__(self, x):
+        return self.diagonal * x
 
 
 class InvertableOperator(object):

pycomplex/stencil/test/test_complex.py
@@ -4,31 +4,35 @@ import numpy.testing as npt
 from pycomplex.stencil.complex import StencilComplex
 
 
-def test_2d():
-    complex = StencilComplex((3, 4))
-    f0 = complex.form(0)
-    f0 = np.arange(12).reshape(1, 3, 4)
-    f1 = complex.dual[0].transpose(f0)
-    f2 = complex.primal[1](f1)
-    npt.assert_allclose(f2, 0)
+def test_chain():
+    """Test that primal and dual chain complexes are closed"""
+    for i in range(2, 5):
+        complex = StencilComplex(tuple(np.arange(i, dtype=np.int) * 2 + 2))
+        print(complex.shape)
+        for n in range(complex.ndim - 1):
+            f0 = complex.form(n)
+            f0[...] = np.random.normal(size=f0.shape)
+            f1 = complex.primal[n](f0)
+            f2 = complex.primal[n+1](f1)
+            npt.assert_allclose(f2, 0, atol=1e-5)
 
-
-def test_3d():
-    complex = StencilComplex((2, 3, 4))
-    f0 = complex.form(0)
-    f0 = np.arange(24).reshape(1, 2, 3, 4)
-    f1 = complex.dual[0].transpose(f0)
-    f2 = complex.primal[1](f1)
-    npt.assert_allclose(f2, 0)
+        for n in range(complex.ndim, 1, -1):
+            f0 = complex.form(n)
+            f0[...] = np.random.normal(size=f0.shape)
+            f1 = complex.dual[n-1](f0)
+            f2 = complex.dual[n-2](f1)
+            npt.assert_allclose(f2, 0, atol=1e-5)
 
 
 def test_transfers():
-    complex = StencilComplex((2, 4))
-    f0 = complex.form(0)
-    f0[...] = 1
-    c0 = complex.coarsen[0](f0)
+    """Test that constant functions are preserved by a roundtrip across scales"""
+    for i in range(2, 5):
+        complex = StencilComplex(tuple(np.arange(i, dtype=np.int) * 2 + 2))
+        print(complex.shape)
 
-    f0 = complex.refine[0](c0)
+        for n in range(complex.ndim + 1):
+            f0 = complex.form(n, init='ones')
+            c0 = complex.coarsen[n](f0)
+            f0_ = complex.refine[n](c0)
 
-    print(c0)
-    print(f0)
+            npt.assert_allclose(f0, f0_)

pycomplex/stencil/util.py
@@ -73,7 +73,7 @@ def binning(arr, steps):
     """inverse of tile"""
     shape = [(a // b, b) for a, b in zip(arr.shape, steps)]
     shape = [c for p in shape for c in p]
-    return arr.reshape(shape).sum(axis=tuple(np.arange(len(steps), dtype=np.int) * 2 + 1))
+    return arr.reshape(shape).mean(axis=tuple(np.arange(len(steps), dtype=np.int) * 2 + 1))
 
 
 def unbinning(arr, output, axes):